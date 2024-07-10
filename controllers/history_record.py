affe4932211f13992b9303cd70dac1cb88d5d28f
dali <hadriandali@gmail.com>
2024-07-09 23:22:55 -0600

    -Feat controlador para historial de registros

---
+++ controllers/history_record.py
@@ -0,0 +1,26 @@
+import sys
+from PySide6.QtWidgets import QApplication, QWidget
+from interface.Ui_main_peso import Ui_MainWindow
+from interface.general_custom_ui import GeneralCustomUi
+from interface.Ui_weighing_units import Ui_DetailWindow as Ui_MainWindow
+
+class HistoryRecordForm(QWidget,Ui_MainWindow):
+    
+    def menuWeighingUnits(self):
+        #self.weighing_units = WeighingUnitsForm()
+        self.weighing_units.show()
+
+
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.ui = GeneralCustomUi(self)
+        # self.config_table()
+        # self.set_table_data()
+
+        self.new_recipe_button_6.clicked.connect(self.menuWeighingUnits)
+        # self.new_recipe_button_3.clicked.connect(self.machine_menu)
+        # self.new_recipe_button_4.clicked.connect(self.part_menu)
+        # self.view_button.clicked.connect(self.view_recipe)
+        
+    
