3c5194ab9b4de23130ac28d69e48a83f081c4a31
dali <hadriandali@gmail.com>
2024-07-11 01:00:16 -0600

    -Feat base de datos de sql lite primera tabla

---
+++ database/SQLite.py
@@ -0,0 +1,30 @@
+import sqlite3
+
+class DatabaseManager:
+    def __init__(self, db_name="datos.db"):
+        self.db_name = db_name
+        self.initialize_db()
+
+    def initialize_db(self):
+        conn = sqlite3.connect(self.db_name)
+        cursor = conn.cursor()
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS registros (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nombre TEXT NOT NULL,
+            peso REAL NOT NULL,
+            descripcion TEXT
+        )
+        ''')
+        conn.commit()
+        conn.close()
+
+    def insert_record(self, nombre, peso, descripcion):
+        conn = sqlite3.connect(self.db_name)
+        cursor = conn.cursor()
+        cursor.execute('''
+        INSERT INTO registros (nombre, peso, descripcion)
+        VALUES (?, ?, ?)
+        ''', (nombre, peso, descripcion))
+        conn.commit()
+        conn.close()
