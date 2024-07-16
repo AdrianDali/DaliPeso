6f4264a85bab24f78b3e6f36e87b52681d8b9c5e
dali <hadriandali@gmail.com>
2024-07-16 15:37:48 -0600

    -Feat agregar residuo al reporte

--- api/add_residue.py
+++ api/add_residue.py
@@ -8,16 +8,18 @@ API_URL = os.getenv("API_URL")
 URL = f"{API_URL}/create-report-residue-user/"
 
 def add_residue(user, report, residue, peso):
+    arr = []
     req = {
-    "user": "khfd@prueba.com",
-    "report": "27",
-    "residue": "PET",
-    "peso": "1",
-    "volumen": "3"
+        "user": user,
+        "report": report,
+        "residue": residue,
+        "peso": peso,
+        "volumen": "3"  # Make sure to adjust this if it's a dynamic value
     }
+    arr.append(req)
     print(req)
     try:
-        response = requests.post(URL, data=req)
+        response = requests.post(URL, json=arr)
         return response.json()
     except Exception as e:
         raise e
